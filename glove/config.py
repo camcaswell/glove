b121a55e5623391312c8693e2593b4adf86bfca5
Cam Caswell <camcaswell@gmail.com>
2022-04-17 13:20:00 -0400

    Make path absolute

--- glove/config.py
+++ glove/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # click.get_app_dir to auto-get an appropriate user config location
 
-GLOVE_INTERNAL = Path("./glove-internal")
+GLOVE_INTERNAL = Path("C:/Code/glove/glove-internal")
 TEMP_DIR = GLOVE_INTERNAL / "temp"
 TEMPLATES_DIR = GLOVE_INTERNAL / "subjects" / "templates"
 META_DIR = GLOVE_INTERNAL / "subjects" / "meta"
