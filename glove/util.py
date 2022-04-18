eb0b89fe2706367b96df735c20322fd4d46fd519
Cam Caswell <camcaswell@gmail.com>
2022-04-17 21:56:49 -0400

    Make list command actually work

--- glove/util.py
+++ glove/util.py
@@ -36,9 +36,24 @@ def get_subject_identifiers() -> dict[str, Optional[str]]:
     """
     subject_identifiers = {}
     for template_filename in os.listdir(cfg.TEMPLATES_DIR):
-        match re.split(r"([-.])", template_filename):
-        case (id_, ".", "glove") if i
-        id_, _, name = template_filename.partition("-")
+        id_, name = parse_template_filename(template_filename)
         subject_identifiers[id_] = name or None
     return subject_identifiers
 
+def parse_template_filename(filename: str) -> tuple[Optional[str], Optional[str]]:
+    """
+    Parse a subject template filename to get the id and name.
+    Can be used to validate as well, since this returns None for the id if invalid. 
+
+    Args:
+        filename (str): Name of a subject template file
+
+    Returns:
+        tuple[Optional[str], Optional[str]]: The id and name of the subject
+    """
+    pat = fr"(?P<id_>{cfg.SUBJECT_ID_PATTERN})(?:-(?P<name>{cfg.SUBJECT_FILE_NAME}))?\.glove$"
+    m = re.match(pat, filename)
+    if m is None:
+        return None, None
+    else:
+        return m.group("id_"),  m.group("name")
