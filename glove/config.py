c52b954d2702b4d69928b8a1eba992817b640685
Cam Caswell <camcaswell@gmail.com>
2022-04-18 00:15:27 -0400

    Add regex for ids and names

--- glove/config.py
+++ glove/config.py
@@ -7,3 +7,7 @@ GLOVE_INTERNAL = Path("C:/Code/glove/glove-internal")
 TEMP_DIR = GLOVE_INTERNAL / "temp"
 TEMPLATES_DIR = GLOVE_INTERNAL / "subjects" / "templates"
 META_DIR = GLOVE_INTERNAL / "subjects" / "meta"
+
+
+SUBJECT_ID_PATTERN = r"[0-9a-fA-F]{8}"
+SUBJECT_FILE_NAME = r"[0-9a-zA-Z\-=._+*?!><,;`'\"\[\]{}|()@#$%^&]+"
